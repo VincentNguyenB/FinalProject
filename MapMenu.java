e3ebf1bcf5cc5ffdbf245ae336871aa3505924cd
VincentNguyenB <61370408+VincentNguyenB@users.noreply.github.com>
2023-04-26 23:07:15 -0700

    Update MapMenu.java

--- MapMenu.java
+++ MapMenu.java
@@ -1,11 +1,12 @@
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class MapMenu {
 
 	
-	private int xCoor = 0;
-	private int yCoor = 0;
+	private int xCoor = 1;
+	private int yCoor = 1;
 	
 	private int difficulty = 1;
 	
@@ -16,24 +17,46 @@ public class MapMenu {
 	
 	MapMenu(Player player)
 	{
-		setXCoor(0);
-		setYCoor(0);
+		setXCoor(1);
+		setYCoor(1);
 		
-		if(player.getDifficulty() == 1)
+		
+		switch(player.getDifficulty())
 		{
-			setMapWidth(3);
-			setMapLength(3);
+			case 1:
+			{
+				setMapWidth(3);
+				setMapLength(3);
+				System.out.println("Generated map is a 3x3 grid \n");
+				
+				break;
+			}
+			case 2:
+			{
+				setMapWidth(4);
+				setMapLength(4);
+				System.out.println("Generated map is a 4x4 grid \n");
+				
+				break;
+			}
+			case 3:
+			{
+				setMapWidth(5);
+				setMapLength(5);
+				System.out.println("Generated map is a 5x5 grid \n");
+				
+				break;
+			}
+			default:
+			{
+				
+			}
+		
+			movementMessage();
+		
 		}
-		else if(player.getDifficulty() == 2)
-		{
-			setMapWidth(4);
-			setMapLength(4);
-		}
-		else if(player.getDifficulty() == 3)
-		{
-			setMapWidth(5);
-			setMapLength(5);
-		}
+		
+		
 		
 	}
 	
@@ -105,11 +128,120 @@ public class MapMenu {
 	
 	public void movement()
 	{
-		System.out.println("enter in the direction you wish to travel to");
+		
+		String direction = "";
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("enter in the direction you wish to travel to. \n");
+		System.out.println("-North ");
+		System.out.println("-South ");
+		System.out.println("-East ");
+		System.out.println("-West ");
+		
+		direction = scan.next();
+		
+		//clearScreen();
+		
+		switch(direction)
+		{
+			case "north":
+			{
+				
+				
+			}
+			case "North":
+			{
+				if(checkWall(getYCoor() + 1) == false)
+				{
+					setYCoor(getYCoor() + 1);
+				}
+				break;
+			}
+			case "south":
+			{
+				
+				
+			}
+			case "South":
+			{
+				if(checkWall(getYCoor() - 1) == false)
+				{
+					setYCoor(getYCoor() - 1);
+				}
+				break;
+			}
+			case "east":
+			{
+				
+				
+			}
+			case "East":
+			{
+				if(checkWall(getXCoor() + 1) == false)
+				{
+					setXCoor(getXCoor() + 1);
+				}
+				break;
+			}
+			case "west":
+			{
+				
+				
+			}
+			case "West":
+			{
+				if(checkWall(getXCoor() - 1) == false)
+				{
+					setXCoor(getXCoor() - 1);
+				}
+				break;
+			}
+			default:
+			{
+				System.out.println("huh ? \n");
+			}
+		
+		}
+		
+		movementMessage();
+		
 		
 		
 	}
 	
+	
+	public boolean checkWall(int value)
+	{
+		
+		
+		if(value <= 0 || value > getMapWidth() || value > getMapLength())
+		{
+			System.out.println("You've hit a wall, idiot. \n");
+			System.out.println("You've taken 5 damage. \n");
+			
+			return true;
+		}
+		
+		
+		return false;
+		
+	}
+	
+	public void movementMessage()
+	{
+		
+		System.out.println("You are now at coordinate (" 
+							+ getXCoor() + ", " 
+							+ getYCoor() + ").");
+
+		
+	}
+	
+	public static void clearScreen()
+	{  
+	    System.out.print("\033[H\033[2J");  
+	    System.out.flush();  
+	}  
 	
 	
 	
@@ -118,8 +250,20 @@ public class MapMenu {
 	public static void main(String[] args) 
 	{
 		
+		
+		Warrior testWarrior = new Warrior("Bob", 1);
+		
+		MapMenu playerMap = new MapMenu(testWarrior);
 				
-		System.out.println("You start off at position (0,0)");
+		boolean dog = true;
+		
+		while(dog == true)
+		{
+			
+			playerMap.movement();
+			
+			
+		}
 		
 		
 		
