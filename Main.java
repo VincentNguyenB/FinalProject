d46c6bdef6b612cd786c55f7b2455ca5fb741035
VincentNguyenB <61370408+VincentNguyenB@users.noreply.github.com>
2023-04-26 23:06:43 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,7 +7,12 @@ public class Main {
 	public static void main(String[] args) 
 	{
 		
-		System.out.println("Hello, Hai");
+		//System.out.println("Hello, Hai");
+		
+		CharacterCreationMenu test = new CharacterCreationMenu();
+		test.start();
+		
+		
 		
 		
 	}
